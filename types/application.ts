3c1bbe43c344ea697f8bcad99e648fe62f28ed0c
leaskn <leask.nicholas@gmail.com>
2026-04-14 18:04:43 -0400

    ECE-APP: add stipend application types

---
+++ types/application.ts
@@ -0,0 +1,37 @@
+export type ReviewTier = 'standard' | 'manual_review';
+
+export type ApplicationInput = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  phoneNumber: string;
+  dateOfBirth: string;
+  ssn: string;
+  addressLine1: string;
+  addressLine2?: string;
+  city: string;
+  state: string;
+  zipCode: string;
+  programName: string;
+  amountRequested: number;
+  agreementAccepted: boolean;
+};
+
+export type StoredApplication = ApplicationInput & {
+  applicationId: string;
+  submittedAt: string;
+  reviewTier: ReviewTier;
+  riskFlags: string[];
+};
+
+export type HandoffRecord = {
+  applicationId: string;
+  applicantName: string;
+  email: string;
+  phoneNumber: string;
+  programName: string;
+  amountRequested: number;
+  reviewTier: ReviewTier;
+  riskFlags: string[];
+  submittedAt: string;
+};
